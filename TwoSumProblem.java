1fd2ba4b1205d89d696be708dc6f359cae77877e
Changing the future! <jayswalshubh@gmail.com>
2018-09-16 14:28:27 +1000

    Update TwoSumProblem.java

--- TwoSumProblem.java
+++ TwoSumProblem.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.util.*;
-
+/* I am using hashMap to solve this problem. But the problem with hashMap is that it doesn't allow redundant keys. 
+So I am still struggling with how to solve problems for inputs such as [3,3] where the hashmap 
+would only add in the recent key which is 3 (according to the code). I know it's confusing but have a look at the code and 
+it should make sense. */
 public class TwoSumProblem {
 
     public int[] twoSum(int[] nums, int target){
